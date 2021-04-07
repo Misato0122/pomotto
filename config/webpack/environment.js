0ec751960a7e220fbb22e93bbceee7a831e9ce31
Misato0122 <mraiokmi3310@gmail.com>
2021-04-07 15:31:22 +0900

    Fix: environment.js修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,13 +2,6 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-   $: 'jquery/src/jquery',
-   jQuery: 'jquery/src/jquery',
-   Popper: ['popper.js', 'default']
-}))
-
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment

4d6b28d2d35986a5bfba870632a52b0a00e41c88
Misato0122 <mraiokmi3310@gmail.com>
2021-04-08 21:38:18 +0900

    Fix:applicartion.html.erb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("bootstrap");
+import './src/application' // application.scss の読み込み
+import 'bootstrap/dist/js/bootstrap'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.

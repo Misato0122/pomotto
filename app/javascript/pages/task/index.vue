e3383054c1261120d6b0f19c7eef22dac0f2db21
Misato0122 <mraiokmi3310@gmail.com>
2021-04-08 14:30:37 +0900

    Fix:マニフェストファイル

--- app/javascript/pages/task/index.vue
+++ app/javascript/pages/task/index.vue
@@ -1,52 +1,11 @@
 <template>
   <div>
-    <div class="text-center">
-      <v-dialog
-        v-model="dialog"
-        width="500"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            color="red lighten-2"
-            dark
-            v-bind="attrs"
-            v-on="on"
-          >
-            Click Me
-          </v-btn>
-        </template>
-  
-        <v-card>
-          <v-card-title class="headline grey lighten-2">
-            Privacy Policy
-          </v-card-title>
-  
-          <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </v-card-text>
-  
-          <v-divider></v-divider>
-  
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              color="primary"
-              text
-              @click="dialog = false"
-            >
-              I accept
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </div>
     <PomodoroTimerBlock
     class="d-flex align-items-center justify-content-center"
     v-if="isVisiblePomodoroTimerBlock"
     @openPomodoroTimerModal="handleOpenPomodoroTimerModal"
     >
     </PomodoroTimerBlock>
-
     <BreakTimerBlock
     class="d-flex align-items-center justify-content-center"
     v-if="isVisibleBreakTimerBlock"
@@ -64,7 +23,6 @@
     >
       タスク作成
     </v-btn>
-
     <TaskList
       task-list-id="todo-list"
       @handleOpenPomodoroTimer="handleOpenPomodoroTimerModal"
@@ -166,7 +124,6 @@ export default {
   name: 'TaskIndex',
   data() {
     return {
-      dialog: false,
       audio: null,
       pomodoroTask: {},
       detailTask: {},

7ae6872dcde6714b442d061160cad78944241fb2
Misato0122 <mraiokmi3310@gmail.com>
2021-04-14 11:30:48 +0900

    Fix:ログアウト機能

--- app/javascript/store/modules/users.js
+++ app/javascript/store/modules/users.js
@@ -43,7 +43,10 @@ export default {
     },
     logoutUser({ commit }) {
       axios.delete(`/session`)
-      commit('setUser', null)
+      .then(res => {
+        commit('setUser', null)
+        router.push("/")
+      })
     },
     updateUser({ commit }, user){
       axios.patch('/profile', {user: user})
